0782a2ab0da9deed5b3709bad7cdeba73b665960
Marufa Akter <marufaakter1947@gmail.com>
2025-09-19 00:19:18 +0600

    Lift up state in Array

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -2,10 +2,14 @@ import React, { use, useState } from 'react';
 import Country from './Country/Country';
 import "./Countries.css"
 
+
 const Countries = ({countriesPromise}) => {
     const [visitedCountries, setVisitedCountries] = useState([]);
     const handleVisitedCountries =(country)=>{
         console.log("Handle visited country clicked",country);
+        const newVisitedCountries =[...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+        // visitedCountries.push(country)
     }
     const countriesData = use(countriesPromise);
     const countries =countriesData.countries
@@ -13,7 +17,12 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>In the countries:{countries.length}</h1>
-            <h3>Total Country Visited: </h3>
+            <h3>Total Country Visited: {visitedCountries.length} </h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                 countries.map(country => <Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
